eabd5859d008ed5e3f5bd4e7eedb3856bfeeefb4
arsahasransu <a.r.sahasransu@gmail.com>
2023-05-09 17:02:06 +0200

    Update to include the new variables in the scouting egamma collection.

--- Analysis/plugins/EGammaOnly_ScoutingNanoAOD.cc
+++ Analysis/plugins/EGammaOnly_ScoutingNanoAOD.cc
@@ -86,10 +86,6 @@ private:
 
   const edm::EDGetTokenT<edm::TriggerResults> trgResultsToken;
 
-  const edm::EDGetTokenT<std::vector<reco::Vertex> > oflpVtxToken;
-  const edm::EDGetTokenT<std::vector<pat::Electron> > oflelectronsToken;
-  const edm::EDGetTokenT<double> oflrhoToken;
-
   const edm::EDGetTokenT<std::vector<Run3ScoutingVertex> > pVtxToken;
   const edm::EDGetTokenT<std::vector<Run3ScoutingElectron> > electronsToken;
   const edm::EDGetTokenT<std::vector<Run3ScoutingPhoton> > photonsToken;
@@ -108,20 +104,6 @@ private:
   Int_t L1_TwoMu, L1_TwoMu_SQOS, L1_HT_ET, L1_OneJet, L1_TwoJet, L1_OneEG, L1_TwoEG;
   Int_t HLT_IsoMu27, HLT_Mu50, DST_Run3PFScouting, DST_HLTMuon_Run3PFScouting, HLT_OtherScoutingPFMonitor;
 
-  // Offline Primary Vertex
-  const static int max_oflpv = 1000;
-  UInt_t n_oflpVtx;
-  vector<Float16_t> oflpVtx_x;
-  vector<Float16_t> oflpVtx_y;
-  vector<Float16_t> oflpVtx_z;
-  vector<Float16_t> oflpVtx_xError;
-  vector<Float16_t> oflpVtx_yError;
-  vector<Float16_t> oflpVtx_zError;
-  vector<Int_t> oflpVtx_trksize;
-  vector<Float16_t> oflpVtx_chi2;
-  vector<Int_t> oflpVtx_ndof;
-  vector<Bool_t> oflpVtx_isvalidvtx;
-        
   // Primary Vertex
   UInt_t n_pVtx;
   vector<Float16_t> pVtx_x;
@@ -135,41 +117,25 @@ private:
   vector<Int_t> pVtx_ndof;
   vector<Bool_t> pVtx_isvalidvtx;
         
-  // Offline Electron
-  UInt_t n_oflele;
-  vector<Float16_t> OflElectron_energy;
-  vector<Float16_t> OflElectron_pt;
-  vector<Float16_t> OflElectron_eta;
-  vector<Float16_t> OflElectron_phi;
-  vector<Float16_t> OflElectron_d0;
-  vector<Float16_t> OflElectron_dz;
-  vector<Float16_t> OflElectron_detain;
-  vector<Float16_t> OflElectron_dphiin;
-  vector<Float16_t> OflElectron_sigmaietaieta;
-  vector<Float16_t> OflElectron_hoe;
-  vector<Float16_t> OflElectron_ooemoop;
-  vector<Int_t> OflElectron_missinghits;
-  vector<Int_t> OflElectron_charge;
-  vector<Float16_t> OflElectron_photoniso;
-  vector<Float16_t> OflElectron_neuthadroniso;
-  vector<Float16_t> OflElectron_chrghadroniso;
-  vector<Bool_t> OflElectron_conversionveto;
-
   // Electron
   UInt_t n_ele;
   vector<Float16_t> Electron_pt;
   vector<Float16_t> Electron_eta;
   vector<Float16_t> Electron_phi;
   vector<Float16_t> Electron_m;
-  vector<Float16_t> Electron_d0;
-  vector<Float16_t> Electron_dz;
+  vector< vector<Float16_t> > Electron_trkpt;
+  vector< vector<Float16_t> > Electron_trketa;
+  vector< vector<Float16_t> > Electron_trkphi;
+  vector< vector<Float16_t> > Electron_trkd0;
+  vector< vector<Float16_t> > Electron_trkdz;
+  vector< vector<Int_t> > Electron_trkcharge;
+  vector< vector<Float16_t> > Electron_trkrchi2;
   vector<Float16_t> Electron_detain;
   vector<Float16_t> Electron_dphiin;
   vector<Float16_t> Electron_sigmaietaieta;
   vector<Float16_t> Electron_hoe;
   vector<Float16_t> Electron_ooemoop;
   vector<Int_t>	Electron_missinghits;
-  vector<Int_t> Electron_charge;
   vector<Float16_t> Electron_ecaliso;
   vector<Float16_t> Electron_hcaliso;
   vector<Float16_t> Electron_tkiso;
@@ -196,10 +162,6 @@ private:
   vector<Float16_t> Photon_seedid;
   vector<Bool_t> Photon_rechitzerosuppression;
 
-  // Offline Rho
-  UInt_t n_oflrhoval;
-  vector<Float16_t> oflrho;
-
   // Rho
   UInt_t n_rhoval;
   vector<Float16_t> rho;
@@ -216,9 +178,6 @@ private:
 
 EGammaOnly_ScoutingNanoAOD::EGammaOnly_ScoutingNanoAOD(const edm::ParameterSet& iConfig): 
   trgResultsToken(consumes<edm::TriggerResults>(iConfig.getParameter<edm::InputTag>("triggerhltres"))),
-  oflpVtxToken(consumes<std::vector<reco::Vertex> >(iConfig.getParameter<edm::InputTag>("oflprimaryVtx"))), 
-  oflelectronsToken(consumes<std::vector<pat::Electron> >(iConfig.getParameter<edm::InputTag>("oflelectrons"))), 
-  oflrhoToken(consumes<double>(iConfig.getParameter<edm::InputTag>("oflrho"))), 
   pVtxToken(consumes<std::vector<Run3ScoutingVertex> >(iConfig.getParameter<edm::InputTag>("primaryVtx"))), 
   electronsToken(consumes<std::vector<Run3ScoutingElectron> >(iConfig.getParameter<edm::InputTag>("electrons"))), 
   photonsToken(consumes<std::vector<Run3ScoutingPhoton> >(iConfig.getParameter<edm::InputTag>("photons"))),
@@ -265,19 +224,6 @@ EGammaOnly_ScoutingNanoAOD::EGammaOnly_ScoutingNanoAOD(const edm::ParameterSet& 
   tree->Branch("DST_HLTMuon_Run3PFScouting", &DST_HLTMuon_Run3PFScouting, "DST_HLTMuon_Run3PFScouting/i" );
   tree->Branch("HLT_OtherScoutingPFMonitor", &HLT_OtherScoutingPFMonitor, "HLT_OtherScoutingPFMonitor/i" );
 
-  // Offline Primary vertex info
-  tree->Branch("n_oflpVtx", &n_oflpVtx, "n_oflpVtx/i");
-  tree->Branch("oflpVtx_x", &oflpVtx_x);
-  tree->Branch("oflpVtx_y", &oflpVtx_y);
-  tree->Branch("oflpVtx_z", &oflpVtx_z);
-  tree->Branch("oflpVtx_xError", &oflpVtx_xError);
-  tree->Branch("oflpVtx_yError", &oflpVtx_yError);
-  tree->Branch("oflpVtx_zError", &oflpVtx_zError);
-  tree->Branch("oflpVtx_trksize", &oflpVtx_trksize);
-  tree->Branch("oflpVtx_chi2", &oflpVtx_chi2);
-  tree->Branch("oflpVtx_ndof", &oflpVtx_ndof);
-  tree->Branch("oflpVtx_isvalidvtx", &oflpVtx_isvalidvtx);
-  
   // Primary vertex info
   tree->Branch("n_pVtx", &n_pVtx, "n_pVtx/i");
   tree->Branch("pVtx_x", &pVtx_x);
@@ -291,41 +237,25 @@ EGammaOnly_ScoutingNanoAOD::EGammaOnly_ScoutingNanoAOD(const edm::ParameterSet& 
   tree->Branch("pVtx_ndof", &pVtx_ndof);
   tree->Branch("pVtx_isvalidvtx", &pVtx_isvalidvtx);
   
-  // Offline Electrons
-  tree->Branch("n_oflele", &n_oflele, "n_oflele/i");
-  tree->Branch("OflElectron_energy", &OflElectron_energy);
-  tree->Branch("OflElectron_pt", &OflElectron_pt);
-  tree->Branch("OflElectron_eta", &OflElectron_eta);
-  tree->Branch("OflElectron_phi", &OflElectron_phi);
-  tree->Branch("OflElectron_d0", &OflElectron_d0);
-  tree->Branch("OflElectron_dz", &OflElectron_dz);
-  tree->Branch("OflElectron_detain", &OflElectron_detain);
-  tree->Branch("OflElectron_dphiin", &OflElectron_dphiin);
-  tree->Branch("OflElectron_sigmaietaieta", &OflElectron_sigmaietaieta);
-  tree->Branch("OflElectron_hoe", &OflElectron_hoe);
-  tree->Branch("OflElectron_ooemoop", &OflElectron_ooemoop);
-  tree->Branch("OflElectron_missinghits", &OflElectron_missinghits);
-  tree->Branch("OflElectron_charge", &OflElectron_charge);
-  tree->Branch("OflElectron_photoniso", &OflElectron_photoniso);
-  tree->Branch("OflElectron_neuthadroniso", &OflElectron_neuthadroniso);
-  tree->Branch("OflElectron_chrghadroniso", &OflElectron_chrghadroniso);
-  tree->Branch("OflElectron_conversionveto", &OflElectron_conversionveto);
-
   // Electrons
   tree->Branch("n_ele", &n_ele, "n_ele/i");
   tree->Branch("Electron_pt", &Electron_pt);
   tree->Branch("Electron_eta", &Electron_eta);
   tree->Branch("Electron_phi", &Electron_phi);
   tree->Branch("Electron_m", &Electron_m);
-  tree->Branch("Electron_d0", &Electron_d0);
-  tree->Branch("Electron_dz", &Electron_dz);
+  tree->Branch("Electron_trkpt", &Electron_trkpt);
+  tree->Branch("Electron_trketa", &Electron_trketa);
+  tree->Branch("Electron_trkphi", &Electron_trkphi);
+  tree->Branch("Electron_trkd0", &Electron_trkd0);
+  tree->Branch("Electron_trkdz", &Electron_trkdz);
+  tree->Branch("Electron_trkcharge", &Electron_trkcharge);
+  tree->Branch("Electron_trkrchi2", &Electron_trkrchi2);
   tree->Branch("Electron_detain", &Electron_detain);
   tree->Branch("Electron_dphiin", &Electron_dphiin);
   tree->Branch("Electron_sigmaietaieta", &Electron_sigmaietaieta);
   tree->Branch("Electron_hoe", &Electron_hoe);
   tree->Branch("Electron_ooemoop", &Electron_ooemoop);
   tree->Branch("Electron_missinghits", &Electron_missinghits);
-  tree->Branch("Electron_charge", &Electron_charge);
   tree->Branch("Electron_ecaliso", &Electron_ecaliso);
   tree->Branch("Electron_hcaliso", &Electron_hcaliso);
   tree->Branch("Electron_tkiso", &Electron_tkiso);
@@ -352,10 +282,6 @@ EGammaOnly_ScoutingNanoAOD::EGammaOnly_ScoutingNanoAOD(const edm::ParameterSet& 
   tree->Branch("Photon_seedid", &Photon_seedid);
   tree->Branch("Photon_rechitzerosuppression", &Photon_rechitzerosuppression);
 
-  // Offline Rho
-  tree->Branch("n_oflrhoval", &n_oflrhoval, "n_oflrhoval/i");
-  tree->Branch("oflrho", &oflrho);
-
   // Rho
   tree->Branch("n_rhoval", &n_rhoval, "n_rhoval/i");
   tree->Branch("rho", &rho);
@@ -373,17 +299,9 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
   edm::Handle<edm::TriggerResults> trigResults;
   iEvent.getByToken(trgResultsToken, trigResults);
 
-  Handle<vector<reco::Vertex> > oflpVtxH;
-  iEvent.getByToken(oflpVtxToken, oflpVtxH);
-  bool oflpVtxValid = oflpVtxH.isValid();
-
   Handle<vector<Run3ScoutingVertex> > pVtxH;
   iEvent.getByToken(pVtxToken, pVtxH);
   bool pVtxValid = pVtxH.isValid();
-
-  Handle<vector<pat::Electron> > oflelectronsH;
-  iEvent.getByToken(oflelectronsToken, oflelectronsH);
-  bool ofleleValid = oflelectronsH.isValid();
 
   Handle<vector<Run3ScoutingElectron> > electronsH;
   iEvent.getByToken(electronsToken, electronsH);
@@ -392,10 +310,6 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
   Handle<vector<Run3ScoutingPhoton> > photonsH;
   iEvent.getByToken(photonsToken, photonsH);
   bool phoValid = photonsH.isValid();
-
-  Handle<double > oflrhoH;
-  iEvent.getByToken(oflrhoToken, oflrhoH);
-  bool oflrhoValid = oflrhoH.isValid();
 
   Handle<double > rhoH;
   iEvent.getByToken(rhoToken, rhoH);
@@ -488,25 +402,7 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
   else {
     HLT_OtherScoutingPFMonitor= 0;
   }  
-  // Offline Primary Vertex
-  n_oflpVtx = 0;
-  if(oflpVtxValid) {
-    for (auto &oflpVtx : *oflpVtxH) {
-      auto *oflpVtx_iter = &oflpVtx;
-      oflpVtx_x.push_back(oflpVtx_iter->x());
-      oflpVtx_y.push_back(oflpVtx_iter->y());
-      oflpVtx_z.push_back(oflpVtx_iter->z());
-      oflpVtx_xError.push_back(oflpVtx_iter->xError());
-      oflpVtx_yError.push_back(oflpVtx_iter->yError());
-      oflpVtx_zError.push_back(oflpVtx_iter->zError());
-      oflpVtx_trksize.push_back(oflpVtx_iter->tracksSize());
-      oflpVtx_chi2.push_back(oflpVtx_iter->chi2());
-      oflpVtx_ndof.push_back(oflpVtx_iter->ndof());
-      oflpVtx_isvalidvtx.push_back(oflpVtx_iter->isValid());
-      n_oflpVtx++;
-    } // end offline primary vertex loop
-  } // end oflpVtxValid condition
-  
+
   // Primary Vertex
   n_pVtx = 0;
   if(pVtxValid) {
@@ -526,33 +422,6 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
     } // end primary vertex loop
   } // end pVtxValid condition
 
-  // Offline Electrons
-  n_oflele = 0;
-  if(ofleleValid) {
-    for (auto &oflele : *oflelectronsH) {
-      auto *oflelectrons_iter = &oflele;
-      OflElectron_energy.push_back(oflelectrons_iter->energy());
-      OflElectron_pt.push_back(oflelectrons_iter->pt());
-      OflElectron_eta.push_back(oflelectrons_iter->eta());
-      OflElectron_phi.push_back(oflelectrons_iter->phi());	
-      OflElectron_d0.push_back(oflelectrons_iter->dB(pat::Electron::PV3D));
-      OflElectron_dz.push_back(oflelectrons_iter->dB(pat::Electron::PVDZ));
-      OflElectron_detain.push_back(oflelectrons_iter->deltaEtaSuperClusterTrackAtVtx());
-      OflElectron_dphiin.push_back(oflelectrons_iter->deltaPhiSuperClusterTrackAtVtx());
-      OflElectron_sigmaietaieta.push_back(oflelectrons_iter->full5x5_sigmaIetaIeta());
-      OflElectron_hoe.push_back(oflelectrons_iter->hadronicOverEm());
-      OflElectron_ooemoop.push_back( (1.0/oflelectrons_iter->correctedEcalEnergy()) - (1.0/oflelectrons_iter->p()) );
-      OflElectron_missinghits.push_back(oflelectrons_iter->gsfTrack()->hitPattern().numberOfLostHits(reco::HitPattern::MISSING_INNER_HITS));
-      OflElectron_charge.push_back(oflelectrons_iter->charge());
-      OflElectron_photoniso.push_back(oflelectrons_iter->photonIso());
-      OflElectron_neuthadroniso.push_back(oflelectrons_iter->neutralHadronIso());
-      OflElectron_chrghadroniso.push_back(oflelectrons_iter->chargedHadronIso());
-      OflElectron_conversionveto.push_back(oflelectrons_iter->passConversionVeto());
-      n_oflele++;
-
-    } // end offline electron loop
-  } // end ofleleValid condition
-
   // Electrons
   n_ele = 0;
   if(eleValid) {
@@ -562,15 +431,19 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
       Electron_eta.push_back(electrons_iter->eta());
       Electron_phi.push_back(electrons_iter->phi());	
       Electron_m.push_back(electrons_iter->m());
-      Electron_d0.push_back(electrons_iter->d0());
-      Electron_dz.push_back(electrons_iter->dz());
+      Electron_trkpt.push_back(electrons_iter->trkpt());
+      Electron_trketa.push_back(electrons_iter->trketa());
+      Electron_trkphi.push_back(electrons_iter->trkphi());	
+      Electron_trkd0.push_back(electrons_iter->trkd0());
+      Electron_trkdz.push_back(electrons_iter->trkdz());
+      Electron_trkcharge.push_back(electrons_iter->trkcharge());
+      Electron_trkrchi2.push_back(electrons_iter->trkchi2overndf());
       Electron_detain.push_back(electrons_iter->dEtaIn());
       Electron_dphiin.push_back(electrons_iter->dPhiIn());
       Electron_sigmaietaieta.push_back(electrons_iter->sigmaIetaIeta());
       Electron_hoe.push_back(electrons_iter->hOverE());	
       Electron_ooemoop.push_back(electrons_iter->ooEMOop());
       Electron_missinghits.push_back(electrons_iter->missingHits());
-      Electron_charge.push_back(electrons_iter->charge());
       Electron_ecaliso.push_back(electrons_iter->ecalIso());
       Electron_hcaliso.push_back(electrons_iter->hcalIso());
       Electron_tkiso.push_back(electrons_iter->trackIso());
@@ -578,12 +451,12 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
       Electron_smin.push_back(electrons_iter->sMin());
       Electron_smaj.push_back(electrons_iter->sMaj());
       Electron_seedid.push_back(electrons_iter->seedId());
+      Electron_rechitzerosuppression.push_back(electrons_iter->rechitZeroSuppression());
       n_ele++;
-
     } // end electron loop
   } // end eleValid condition
 
-  // Muons
+  // Photons
   n_pho = 0;  
   if(phoValid) {
 
@@ -607,13 +480,6 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
     } // end photon loop
   } // end phoValid condition
 
-  // Offline Rho
-  n_oflrhoval = 0;
-  if(oflrhoValid) {
-    oflrho.push_back(*oflrhoH);
-    n_oflrhoval++;
-  }
-
   // Rho
   n_rhoval = 0;
   if(rhoValid) {
@@ -627,16 +493,6 @@ void EGammaOnly_ScoutingNanoAOD::analyze(const edm::Event& iEvent, const edm::Ev
 
 void EGammaOnly_ScoutingNanoAOD::clearVars(){
   l1Result_.clear();
-  oflpVtx_x.clear();
-  oflpVtx_y.clear();
-  oflpVtx_z.clear();
-  oflpVtx_xError.clear();
-  oflpVtx_yError.clear();
-  oflpVtx_zError.clear();
-  oflpVtx_trksize.clear();
-  oflpVtx_chi2.clear();
-  oflpVtx_ndof.clear();
-  oflpVtx_isvalidvtx.clear();
   pVtx_x.clear();
   pVtx_y.clear();
   pVtx_z.clear();
@@ -647,36 +503,23 @@ void EGammaOnly_ScoutingNanoAOD::clearVars(){
   pVtx_chi2.clear();
   pVtx_ndof.clear();
   pVtx_isvalidvtx.clear();
-  OflElectron_energy.clear();
-  OflElectron_pt.clear();
-  OflElectron_eta.clear();
-  OflElectron_phi.clear();
-  OflElectron_d0.clear();
-  OflElectron_dz.clear();
-  OflElectron_detain.clear();
-  OflElectron_dphiin.clear();
-  OflElectron_sigmaietaieta.clear();
-  OflElectron_hoe.clear();
-  OflElectron_ooemoop.clear();
-  OflElectron_missinghits.clear();
-  OflElectron_charge.clear();
-  OflElectron_photoniso.clear();
-  OflElectron_neuthadroniso.clear();
-  OflElectron_chrghadroniso.clear();
-  OflElectron_conversionveto.clear();
   Electron_pt.clear();
   Electron_eta.clear();
   Electron_phi.clear();
   Electron_m.clear();
-  Electron_d0.clear();
-  Electron_dz.clear();
+  Electron_trkpt.clear();
+  Electron_trketa.clear();
+  Electron_trkphi.clear();
+  Electron_trkd0.clear();
+  Electron_trkdz.clear();
+  Electron_trkcharge.clear();
+  Electron_trkrchi2.clear();
   Electron_detain.clear();
   Electron_dphiin.clear();
   Electron_sigmaietaieta.clear();
   Electron_hoe.clear();
   Electron_ooemoop.clear();
   Electron_missinghits.clear();
-  Electron_charge.clear();
   Electron_ecaliso.clear();
   Electron_hcaliso.clear();
   Electron_tkiso.clear();
@@ -698,7 +541,6 @@ void EGammaOnly_ScoutingNanoAOD::clearVars(){
   Photon_smin.clear();
   Photon_smaj.clear();
   Photon_seedid.clear();
-  oflrho.clear();
   rho.clear();
 }
 
